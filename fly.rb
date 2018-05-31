f13a395a6206b4cd3d3e6438c7c31c1d64dc22cd
Anura team account <anura@groups.springer.com>
2018-05-31 09:08:26 +0100

    [Hilverd] Update SHA256 checksum after Concourse update

--- fly.rb
+++ fly.rb
@@ -3,7 +3,7 @@ class Fly < Formula
   homepage "https://github.com/springernature/halfpipe"
   version "3.9.2"
   url "https://concourse.halfpipe.io/api/v1/cli?arch=amd64&platform=darwin"
-  sha256 "9f345192478b74f7764829c7be16bab2b4161d15308caa7d064aac7ab150d5ea"
+  sha256 "4a47a960d6d75612374bba1b8348c18caeb63c8368efba5911e8824021669df6"
 
   def install
     mv "cli", "fly"
