bc017573b27d73fadfb563cb907aee50fdb07cdb
James Shiell <james@infernus.org>
2018-06-11 12:56:46 +0100

    Changed fly source to use Concourse on GitHub, to hopefully stop it breaking every time our local Concourse is updated

--- fly.rb
+++ fly.rb
@@ -2,7 +2,7 @@ class Fly < Formula
   desc "halfpipe cli"
   homepage "https://github.com/springernature/halfpipe"
   version "3.14.1"
-  url "https://concourse.halfpipe.io/api/v1/cli?arch=amd64&platform=darwin"
+  url "https://github.com/concourse/concourse/releases/download/v#{version}/fly_darwin_amd64"
   sha256 "61fd5fe1ac796abe12b386d126a4bb876ba06d587f0a35d2b0f1186487e1f1ca"
 
   def install
