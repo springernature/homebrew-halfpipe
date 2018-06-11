83af175c607ea8bbca80ce7c19f42aa378f62b9d
James Shiell <james@infernus.org>
2018-06-11 12:52:38 +0100

    Updated fly to 3.14.1

--- fly.rb
+++ fly.rb
@@ -1,9 +1,9 @@
 class Fly < Formula
   desc "halfpipe cli"
   homepage "https://github.com/springernature/halfpipe"
-  version "3.9.2"
+  version "3.14.1"
   url "https://concourse.halfpipe.io/api/v1/cli?arch=amd64&platform=darwin"
-  sha256 "4a47a960d6d75612374bba1b8348c18caeb63c8368efba5911e8824021669df6"
+  sha256 "61fd5fe1ac796abe12b386d126a4bb876ba06d587f0a35d2b0f1186487e1f1ca"
 
   def install
     mv "cli", "fly"
