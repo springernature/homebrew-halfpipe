42976807add89f255a362d15e8b4abfc8a368f00
Jamie Shiell <jamie@infernus.org>
2019-04-02 08:27:05 +0100

    Updated for new artefact filename

--- fly.rb
+++ fly.rb
@@ -2,11 +2,11 @@ class Fly < Formula
   desc "halfpipe cli"
   homepage "https://github.com/springernature/halfpipe"
   version "5.0.1"
-  url "https://github.com/concourse/concourse/releases/download/v#{version}/fly_darwin_amd64.tgz"
+  url "https://github.com/concourse/concourse/releases/download/v#{version}/fly-#{version}-darwin-amd64.tgz"
   sha256 "3894b059d947f9a39705eaddbac3ab8ea35dd4bd826e3154f4e0633547960a45"
 
   def install
-    tar xzf "fly_darwin_amd64"
+    tar xzf "fly-#{version}-darwin-amd64.tgz"
     bin.install "fly"
   end
 
