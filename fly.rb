980090ced3d0b02f09cb0487a98123bb2f9f77fa
Jamie Shiell <jamie@infernus.org>
2019-04-02 08:25:12 +0100

    Updated for new artefact format for Concourse 5

--- fly.rb
+++ fly.rb
@@ -2,11 +2,11 @@ class Fly < Formula
   desc "halfpipe cli"
   homepage "https://github.com/springernature/halfpipe"
   version "5.0.1"
-  url "https://github.com/concourse/concourse/releases/download/v#{version}/fly_darwin_amd64"
+  url "https://github.com/concourse/concourse/releases/download/v#{version}/fly_darwin_amd64.tgz"
   sha256 "3894b059d947f9a39705eaddbac3ab8ea35dd4bd826e3154f4e0633547960a45"
 
   def install
-    mv "fly_darwin_amd64", "fly"
+    tar xzf "fly_darwin_amd64"
     bin.install "fly"
   end
 
