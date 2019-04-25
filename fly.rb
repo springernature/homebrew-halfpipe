3da61445e906d7d860c8c86d134b6e212f86e80c
Jamie Shiell <jamie@infernus.org>
2019-04-25 08:42:55 +0100

    Really fixed shasum for Fly archive, not binary :facepalm:

--- fly.rb
+++ fly.rb
@@ -3,7 +3,7 @@ class Fly < Formula
   homepage "https://github.com/springernature/halfpipe"
   version "5.1.0"
   url "https://github.com/concourse/concourse/releases/download/v#{version}/fly-#{version}-darwin-amd64.tgz"
-  sha256 "d58fbc5f412dc991821819bc7eee9dfffed4104fa698e0e021d8cccc2899870b"
+  sha256 "d0fb842725636536355c67821e7a7fe2be90a41299ae1324b747f50cd88ac9c8"
 
   def install
     bin.install "fly"
