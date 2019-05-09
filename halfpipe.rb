3c38b221226405d04856ae89f00317cf1fbcdd96
Jamie Shiell <jamie@infernus.org>
2019-05-09 15:49:11 +0100

    Updated Halfpipe to 3.2.0

--- halfpipe.rb
+++ halfpipe.rb
@@ -1,9 +1,9 @@
 class Halfpipe < Formula
   desc "halfpipe cli"
   homepage "https://github.com/springernature/halfpipe"
-  version "3.1.0"
+  version "3.2.0"
   url "https://github.com/springernature/halfpipe/releases/download/#{version}/halfpipe_darwin_#{version}"
-  sha256 "e23270fd7af1990b6d93c238ffd69ff10913275f57ff2440505b89ba2ede94df"
+  sha256 "91723489a8af1556434ee0f0d77b1ae7c8ede909cdfd7942061a894ccc1706f0"
 
   def install
     mv "halfpipe_darwin_#{version}", "halfpipe"
