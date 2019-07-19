859c910d824f24e715ebdd14ad7eb4d7226b079d
Jamie Shiell <jamie@infernus.org>
2019-07-19 12:06:39 +0100

    Fixed sha for fly

--- fly.rb
+++ fly.rb
@@ -3,7 +3,7 @@ class Fly < Formula
   homepage "https://github.com/springernature/halfpipe"
   version "5.3.0"
   url "https://github.com/concourse/concourse/releases/download/v#{version}/fly-#{version}-darwin-amd64.tgz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "33416ed108a45fe7462947914e3ac0b3a5372854ef1b621ae1cc19676fb1e375"
 
   def install
     bin.install "fly"
