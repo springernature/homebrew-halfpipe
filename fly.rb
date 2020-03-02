6ec81d7535296255db7c842658cc4d323784107d
Jamie Shiell <jamie@infernus.org>
2020-03-02 09:17:07 +0000

    Updated halfpipe/fly

--- fly.rb
+++ fly.rb
@@ -1,9 +1,9 @@
 class Fly < Formula
   desc "halfpipe cli"
   homepage "https://github.com/springernature/halfpipe"
-  version "5.5.1"
+  version "5.7.0"
   url "https://github.com/concourse/concourse/releases/download/v#{version}/fly-#{version}-darwin-amd64.tgz"
-  sha256 "1ea4bdc057270c9b58411c5750d65014b9931d541ca257f260676e25e55351d4"
+  sha256 "200985118ef97f51e63322e960740a96f805d974e00261ede35cc2edea4c6653"
 
   def install
     bin.install "fly"
--- halfpipe.rb
+++ halfpipe.rb
@@ -1,9 +1,9 @@
 class Halfpipe < Formula
   desc "halfpipe cli"
   homepage "https://github.com/springernature/halfpipe"
-  version "3.17.0"
+  version "3.27.0"
   url "https://github.com/springernature/halfpipe/releases/download/#{version}/halfpipe_darwin_#{version}"
-  sha256 "6cd14793a5be69533e6828168269e79ee5aeda968c7d53314a70830aec7a004f"
+  sha256 "c4bd1fb6edb5629de1176e8e25af0140cb37e92fc63c1e0591393eb3c3b72d2a"
 
   def install
     mv "halfpipe_darwin_#{version}", "halfpipe"
