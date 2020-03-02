9d567fa47538f0b6522982e1fa5345e58699925a
Jamie Shiell <jamie@infernus.org>
2020-03-02 09:17:54 +0000

    Corrected hash, as it should be the archive hash, not the binary 🤦‍♀️

--- fly.rb
+++ fly.rb
@@ -3,7 +3,7 @@ class Fly < Formula
   homepage "https://github.com/springernature/halfpipe"
   version "5.7.0"
   url "https://github.com/concourse/concourse/releases/download/v#{version}/fly-#{version}-darwin-amd64.tgz"
-  sha256 "200985118ef97f51e63322e960740a96f805d974e00261ede35cc2edea4c6653"
+  sha256 "efa0d9760ac0c6b02088b2c94a0389e62cacb6fddd55775b41661dcfd92ff856"
 
   def install
     bin.install "fly"
