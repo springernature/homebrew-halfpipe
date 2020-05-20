a008ff382dddc6d4fc638a660773bdec7a303e17
Jamie Shiell <jamie@infernus.org>
2020-05-20 16:38:24 +0100

    I remain incompetent, fixed hash

--- halfpipe.rb
+++ halfpipe.rb
@@ -3,7 +3,7 @@ class Halfpipe < Formula
   homepage "https://github.com/springernature/halfpipe"
   version "3.31.1"
   url "https://github.com/springernature/halfpipe/releases/download/#{version}/halfpipe_darwin_#{version}"
-  sha256 "f49b51786fafc9ca5e790ce72f94df4ca211e9d563887469f33edaf209ab8ebe"
+  sha256 "a6429444d4c39997e951d2d3e34154bedb5b17002b0f9d86ade03dd83c378a87"
 
   def install
     mv "halfpipe_darwin_#{version}", "halfpipe"
