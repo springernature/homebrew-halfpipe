21d690274c1cccd73e87ea36ba7e3518af4f4b31
Jamie Shiell <jamie@infernus.org>
2021-10-21 13:28:57 +0100

    Fixed URI

--- halfpipe.rb
+++ halfpipe.rb
@@ -2,7 +2,7 @@ class Halfpipe < Formula
   desc "halfpipe cli"
   homepage "https://github.com/springernature/halfpipe"
   version "3.78.0"
-  url "https://springernature.jfrog.io/ui/native/halfpipe/halfpipe_darwin_#{version}"
+  url "https://springernature.jfrog.io/artifactory/halfpipe/halfpipe_darwin_#{version}"
   sha256 "a1b643d953658d56a177c101279dca2ec10583fc500bccecb31fb2f5e4b26bd7"
 
   def install
