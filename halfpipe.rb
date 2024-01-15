982198a364574b10b740bd13d9904328ea04b522
James Shiell <james.shiell@springer.com>
2024-01-15 08:22:18 +0000

    Update to 3.132.0

--- halfpipe.rb
+++ halfpipe.rb
@@ -1,9 +1,9 @@
 class Halfpipe < Formula
   desc "halfpipe cli"
   homepage "https://github.com/springernature/halfpipe"
-  version "3.127.0"
+  version "3.132.0"
   url "https://springernature.jfrog.io/artifactory/halfpipe/halfpipe_darwin_#{version}"
-  sha256 "e029b8f541452bcc5381f65b59091db9e2f6dfbd9f5d2b05f9d7b1e6c328b71a"
+  sha256 "8fb27ad54f26986574d8bd38d8c3c1288bab391cc91e93fdf023ef8cd71a9c2b"
 
   def install
     mv "halfpipe_darwin_#{version}", "halfpipe"
