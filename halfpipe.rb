73c17706f93f64d3700adc64cf2887f548270aa6
James Shiell <james.shiell@springer.com>
2025-12-08 08:51:09 +0000

    Whoops, wrong checksum

--- halfpipe.rb
+++ halfpipe.rb
@@ -3,7 +3,7 @@ class Halfpipe < Formula
   homepage "https://github.com/springernature/halfpipe"
   version "3.186.0"
   url "https://springernature.jfrog.io/artifactory/halfpipe/halfpipe_darwin_#{version}"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "8f0942307801cb5ae24cbc9c8fe4254c7f0e5b7fb04642af563c7472b9597da8"
 
   def install
     mv "halfpipe_darwin_#{version}", "halfpipe"
