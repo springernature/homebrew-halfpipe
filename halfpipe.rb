973646a053b9ee0f88709ab1d7812df048b33928
Halfpipe CI <halfpipe@springernature.com>
2026-04-29 11:05:54 +0000

    Update halfpipe to 3.226.0

--- halfpipe.rb
+++ halfpipe.rb
@@ -1,9 +1,9 @@
 class Halfpipe < Formula
   desc "halfpipe cli"
   homepage "https://github.com/springernature/halfpipe"
-  version "3.225.0"
+  version "3.226.0"
   url "https://github.com/springernature/halfpipe/releases/download/#{version}/halfpipe_darwin_#{version}"
-  sha256 "2835051a509827b346fd586be348c83fcd7efc567f6fad0afce984b5282cf789"
+  sha256 "da8ef3ec86f3549f026817bf31e563e05bc0dd04ecb0eff052aeb88f84fa609a"
 
   def install
     mv "halfpipe_darwin_#{version}", "halfpipe"
